6bc5cdda85cc00a262e565f77b959d78f178ff99
zecolmeia <zecolmeia@teste.com>
2022-02-02 10:04:02 -0300

    Adicionar arquivos de projeto.

---
+++ Aulas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Aulas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, res;
+            res=n1 = n2 = n3 = n4 = 0;
+            string resultado;
+
+            //capturando dados
+            Console.Write("Digite a nota 1:");      
+            n1=int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 2:");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 3:");
+            n3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a nota 4:");
+            n4 = int.Parse(Console.ReadLine());
+
+            res = n1 + n2 + n3 + n4;
+
+            if (res >= 60)
+            {
+                resultado = "Aprovado";
+            }
+            else
+            {
+                resultado = "Reprovado";
+            }
+
+            //os índices dentro {} é a posição da concatenação
+            Console.WriteLine("Nota: {0} - Resultado:{1}", res, resultado);
+        }
+    }
+}
