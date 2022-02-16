00163afad1394cbf750e6cb433aeeeb32ee5199a
zecolmeia <zecolmeia@teste.com>
2022-02-16 16:04:10 -0300

    att

---
+++ Ex1For/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex1For {
+    class Program {
+        static void Main(string[] args) {
+            //Exibir os número ímpares de um número até X;
+            Console.WriteLine("Digite um número: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++) {
+                if (i % 2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
